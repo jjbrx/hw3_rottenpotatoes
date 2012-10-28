ffbef263f12a3cb431e4bec62c764e12668a182b
saasbook <saasbook@saasbook.(none)>
2012-10-28 16:46:11 -0700

    hw3 part 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create!(movie)
+    Movie.create!(movie) # unless Movie.where(:title => movie[:title])
   end
   #flunk "Unimplemented"
 end
@@ -15,7 +15,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ /(#{e1}).*(#{e2})/m
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,4 +27,17 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,/).each do |rating|
+    rating = rating.strip
+    if not uncheck then
+      step %{I check "ratings_#{rating}"}
+    else
+      step %{I uncheck "ratings_#{rating}"}
+    end
+  end
 end
+
+Then /I should see all of the movies/ do
+  assert page.has_xpath?("//table[@id=\"movies\"]/tbody/tr", :count => Movie.count)
+end
+
